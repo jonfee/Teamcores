c60b6aed3029d45f4cabeb78c27fb417b81e7ec9
廖武斌 <liaowubinf@163.com>
2017-07-26 00:17:03 +0800

    新增用户接口及页面

--- TeamCores.Web/Api/UserController.cs
+++ TeamCores.Web/Api/UserController.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+
+using TeamCores.Domain.Models;
 using TeamCores.Domain.Services;
 using TeamCores.Misc.Controller;
-using TeamCores.Models;
 using TeamCores.Web.ViewModel.User;
 
 namespace TeamCores.Web.Api
@@ -9,40 +10,50 @@ namespace TeamCores.Web.Api
     [Route("api/User")]
     public class UserController : BaseController
     {
-		[HttpPost]
-		[Route("search")]
-		public IActionResult Search(UserSearcherViewModel searcher)
-		{
-			if (searcher == null)
-			{
-				searcher = new UserSearcherViewModel
-				{
-					PageIndex = 1,
-					PageSize = 10
-				};
-			}
+        [HttpPost]
+        [Route("search")]
+        public IActionResult Search(UserSearcherViewModel searcher)
+        {
+            if (searcher == null)
+            {
+                searcher = new UserSearcherViewModel
+                {
+                    PageIndex = 1,
+                    PageSize = 10
+                };
+            }
 
-			var result = new UserService().Search(searcher.PageSize, searcher.PageIndex, searcher.Keyword, searcher.Status);
+            var result = new UserService().Search(searcher.PageSize, searcher.PageIndex, searcher.Keyword,
+                searcher.Status);
 
-			return Ok(result);
-		}
+            return Ok(result);
+        }
 
-		[HttpPost]
-		[Route("setenabled")]
-		public IActionResult SetEnabled(long userId)
-		{
-			new UserService().SetEnabled(userId);
+        [HttpPost]
+        [Route("setenabled")]
+        public IActionResult SetEnabled(long userId)
+        {
+            new UserService().SetEnabled(userId);
 
-			return Ok(true);
-		}
+            return Ok(true);
+        }
 
-		[HttpPost]
-		[Route("setdisabled")]
-		public IActionResult SetDisabled(long userId)
-		{
-			new UserService().SetDisabled(userId);
+        [HttpPost]
+        [Route("setdisabled")]
+        public IActionResult SetDisabled(long userId)
+        {
+            new UserService().SetDisabled(userId);
 
-			return Ok(true);
-		}
-	}
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(NewUser user)
+        {
+            new UserService().AddUser(user);
+
+            return Ok(true);
+        }
+    }
 }
