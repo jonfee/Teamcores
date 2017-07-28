b8b4d6af0b9e187cb755bd06d9905de0bbeea97f
jonfee <337883612@qq.com>
2017-07-28 16:56:34 +0800

    +题目状态及题型枚举

---
+++ TeamCores.Domain/Enums/QuestionStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace TeamCores.Domain.Enums
+{
+	/// <summary>
+	/// 题目状态枚举
+	/// </summary>
+	public enum QuestionStatus
+    {
+		/// <summary>
+		/// 启用
+		/// </summary>
+		[Description("启用")]
+		ENABLED = 1,
+		/// <summary>
+		/// 禁用
+		/// </summary>
+		[Description("禁用")]
+		DISABLED = 0
+	}
+}
