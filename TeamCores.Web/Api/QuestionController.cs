94cde2cd7a520a7c634c2a3a4e6ce677eba8fab1
jonfee <337883612@qq.com>
2017-07-30 12:41:58 +0800

    完成“添加题目"接口服务功能

--- TeamCores.Web/Api/QuestionController.cs
+++ TeamCores.Web/Api/QuestionController.cs
@@ -5,11 +5,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TeamCores.Misc.Controller;
+using TeamCores.Web.ViewModel.Question;
+using TeamCores.Domain.Services;
+using TeamCores.Misc;
 
 namespace TeamCores.Web.Api
 {
     [Route("api/Question")]
     public class QuestionController : BaseController
     {
+        QuestionService service = null;
+
+        public QuestionController()
+        {
+            service = new QuestionService();
+        }
+
+        /// <summary>
+        /// 新增题目
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(NewQuestionViewModel model)
+        {
+            var success = service.Add(
+                Utility.GetUserContext().UserId,
+                model.CourseId,
+                model.SubjectId,
+                model.Type,
+                model.Marking,
+                model.Topic,
+                model.AnswerOptions);
+
+            return Ok(success);
+        }
     }
 }
---
+++ TeamCores.Web/ViewModel/Question/NewQuestionViewModel.cs
@@ -0,0 +1,40 @@
+﻿using TeamCores.Domain.Models.Answer;
+
+namespace TeamCores.Web.ViewModel.Question
+{
+    /// <summary>
+    /// 新题目视图模型
+    /// </summary>
+    public class NewQuestionViewModel
+    {
+        /// <summary>
+        /// 归属课程ID
+        /// </summary>
+        public long CourseId { get; set; }
+
+        /// <summary>
+        /// 科目ID
+        /// </summary>
+        public long SubjectId { get; set; }
+
+        /// <summary>
+        /// 课程类型（单选，多选，对错，填空题，问答题）
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// 是否需要阅卷
+        /// </summary>
+        public bool Marking { get; set; }
+
+        /// <summary>
+        /// 题目
+        /// </summary>
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// 原始答案备选项
+        /// </summary>
+        public QuestionAnswer AnswerOptions { get;set; }
+    }
+}
