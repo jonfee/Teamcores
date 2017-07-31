2047b41ecca835c1d33f478e19a9581a4f3b9129
jonfee <337883612@qq.com>
2017-07-31 16:30:53 +0800

    添加“学习计划状态”及“用户学习计划状态“枚举

---
+++ TeamCores.Domain/Enums/StudyPlanStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace TeamCores.Domain.Enums
+{
+	/// <summary>
+	/// 学习计划状态 枚举
+	/// </summary>
+	public enum StudyPlanStatus
+    {
+		/// <summary>
+		/// 启用
+		/// </summary>
+		[Description("启用")]
+		ENABLED = 1,
+		/// <summary>
+		/// 禁用
+		/// </summary>
+		[Description("禁用")]
+		DISABLED = 0
+	}
+}
---
+++ TeamCores.Domain/Enums/UserStudyPlanStatus.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace TeamCores.Domain.Enums
+{
+	/// <summary>
+	/// 用户学习计划状态 枚举
+	/// </summary>
+	public enum UserStudyPlanStatus
+    {
+		/// <summary>
+		/// 未开始
+		/// </summary>
+		[Description("未开始")]
+		NOTSTARTED =1,
+		/// <summary>
+		/// 学习中
+		/// </summary>
+		[Description("学习中")]
+		STUDYING,
+		/// <summary>
+		/// 已完成
+		/// </summary>
+		[Description("已完成")]
+		COMPLETE
+	}
+}
