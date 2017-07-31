07ef8805bbb3697940aa117d24c4ff0a719b2a80
jonfee <337883612@qq.com>
2017-07-31 18:09:28 +0800

    新学习计划领域业务处理（未完成）

--- TeamCores.Data/DataAccess/StudyPlanAccessor.cs
+++ TeamCores.Data/DataAccess/StudyPlanAccessor.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TeamCores.Data.DataAccess
 {
-    class StudyPlanAccessor
-    {
-    }
+	/// <summary>
+	/// 学习计划仓储服务
+	/// </summary>
+	public static class StudyPlanAccessor
+	{
+		/// <summary>
+		/// 检测学习计划标题是否已存在
+		/// </summary>
+		/// <param name="title"></param>
+		/// <returns></returns>
+		public static bool Exists(string title)
+		{
+			if (string.IsNullOrWhiteSpace(title)) return false;
+
+			using (var db = new DataContext())
+			{
+				return db.StudyPlan.Count(p => string.Compare(title, p.Title, true) == 0) > 0;
+			}
+		}
+	}
 }
