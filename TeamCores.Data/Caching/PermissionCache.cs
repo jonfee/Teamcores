de230e58aae8d77cc958eb2973818105063449de
jonfee <337883612@qq.com>
2017-08-11 18:02:22 +0800

    权限缓存类编写

---
+++ TeamCores.Data/Caching/PermissionCache.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using TeamCores.Data.DataAccess;
+using TeamCores.Data.Entity;
+
+namespace TeamCores.Data.Caching
+{
+	/// <summary>
+	/// 权限缓存
+	/// </summary>
+	public sealed class PermissionCache : DataCache<Permission>
+	{
+		private static PermissionCache _instance;
+
+		public static PermissionCache Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (my_locker)
+					{
+						if (_instance == null)
+						{
+							return new PermissionCache();
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+		private PermissionCache() { }
+
+		protected override Dictionary<object, Permission> GetInitData()
+		{
+			var list = PermissionAccessor.GetAll();
+
+			var dic = new Dictionary<object, Permission>();
+
+			if (list != null)
+			{
+				foreach (var item in list)
+				{
+					dic.Add(item.Id, item);
+				}
+			}
+
+			return dic;
+		}
+	}
+}
---
+++ TeamCores.Data/DataAccess/PermissionAccessor .cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TeamCores.Data.Entity;
+
+namespace TeamCores.Data.DataAccess
+{
+	/// <summary>
+	/// 权限仓储服务
+	/// </summary>
+	public static class PermissionAccessor
+	{
+		/// <summary>
+		///  获取所有权限
+		/// </summary>
+		/// <returns></returns>
+		public static List<Permission> GetAll()
+		{
+			using (var db = new DataContext())
+			{
+				return (from p in db.Permission
+						select p).ToList();
+			}
+		}
+	}
+}
